99d564e8f7beb43c0b0ae038732d6150283efca9
houfujian <ApeLiftingIron@163.com>
2019-06-14 22:40:19 +0800

    完成get相关方法

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,13 +1,37 @@
-import { AxiosPromise, AxiosRequestConfig } from "../types";
+import { AxiosPromise, AxiosRequestConfig, Method } from "../types";
 import {dispatchRequest} from './dispatchRequest';
+
 class Axios {
   request(config: AxiosRequestConfig): AxiosPromise {
     return dispatchRequest(config)
   }
 
-  get(config: AxiosRequestConfig, url: string): AxiosPromise {
+  get(url: string, config: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithoutData("get", url, config)
+  }
+
+  delete(url: string, config: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithoutData("delete", url, config)
+  }
+
+  head(url: string, config: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithoutData("head", url, config)
+  }
+
+  options(url: string, config: AxiosRequestConfig): AxiosPromise {
+    return this._requestMethodWithoutData("options", url, config)
+  }
+
+  // post(){}
+
+  // put(){}
+
+  // patch(){}
+
+
+  _requestMethodWithoutData(method: Method, url: string, config: AxiosRequestConfig) {
     return dispatchRequest(Object.assign({}, config, {
-      method: "get",
+      method,
       url
     }))
   }
