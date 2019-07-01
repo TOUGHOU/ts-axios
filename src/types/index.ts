97700135565e248f7f2f1c93a6817416718d5f94
houfujian <ApeLiftingIron@163.com>
2019-07-01 22:59:05 +0800

    interceptorManager定义interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -70,3 +70,17 @@ export interface AxiosInstance extends Axios {
 
   <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
+
+export interface AxiosInterceptorManager<T> {
+  use(resolve: ResolvedFn<T>, reject: RejectedFn): number
+
+  eject(id: number): void
+}
+
+export interface ResolvedFn<T> {
+  (val: T): T | Promise<T>
+}
+
+export interface RejectedFn {
+  (error: any): any
+}
