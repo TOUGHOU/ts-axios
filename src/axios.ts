b25487ece9cbdc12eca3596c8d726dc328bb8e77
houfujian <ApeLiftingIron@163.com>
2020-06-18 09:03:18 +0800

    axios.create  实现

--- src/axios.ts
+++ src/axios.ts
@@ -1,17 +1,24 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/utils'
 import defaults from './defaults'
+import mergeConfig from './core/mergeConfig'
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
 
   extend(instance, context)
 
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+
+axios.create = function create(config) {
+  config = mergeConfig(defaults, config)
+
+  return createInstance(config)
+}
 
 export default axios
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import Axios from '../core/Axios'
+
 export type Method =
   | 'get'
   | 'GET'
@@ -75,6 +77,10 @@ export interface AxiosInstance extends Axios {
   <T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
   <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+}
+
+export interface AxiosStatic extends AxiosInstance {
+  create(config: AxiosRequestConfig): AxiosInstance
 }
 
 export interface AxiosInterceptorManager<T> {
